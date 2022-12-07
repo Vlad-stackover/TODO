ced174c3d9b86154df7c3d877a2fdec33e2b9ba9
Vlad <strixus480@gmail.com>
2022-12-07 17:07:17 +0100

    v0.0.3

--- todo_list/main/urls.py
+++ todo_list/main/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.login),
-    path('main', views.index),
+    path('', views.login, name='login'),
+    path('main', views.index, name='main'),
+    path('about', views.about, name='about'),
 
 ]
--- todo_list/main/views.py
+++ todo_list/main/views.py
@@ -6,3 +6,6 @@ def index(request):
 
 def login(request):
     return render(request, 'main/login.html')
+
+def about(request):
+    return render(request, 'main/about.html')
