336ad1b9faac748056960b4b6765940bb19bc43f
Vlad <strixus480@gmail.com>
2022-12-18 23:13:14 +0100

    v0.0.7

--- todo_list/main/models.py
+++ todo_list/main/models.py
@@ -7,12 +7,13 @@ class Task(models.Model):
     title = models.CharField('Name', max_length=50)
     description = models.TextField('description', null=True, blank=True)
     complete = models.BooleanField(default=False)
-    # created = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add = True)
 
     def __str__(self):
         return self.title
 
 
-    # class Meta:
-    #     ordering = ['complete']
+    class Meta:
+
+        ordering = ['title', 'complete']
 
--- todo_list/main/views.py
+++ todo_list/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from .models import Task
+from .forms import TodoForm
 
 def index(request):
     tasks = Task.objects.all()
@@ -11,3 +12,29 @@ def login(request):
 
 def about(request):
     return render(request, 'main/about.html')
+
+def todo_list(request):
+    todo_items = Task.objects.all().order_by('created_at')
+    form = TodoForm()
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('todo_list')
+
+    context = {'todo_items': todo_items}
+    return render(request, 'main/base.html', context)
+
+def update_todo(request, pk):
+    todo_item = Task.objects.get(id=pk)
+    form = TodoForm(instance=todo_item)
+
+    if request.method == 'POST':
+        form = TodoForm(request.POST, instance=todo_item)
+        if form.is_valid():
+            form_save()
+            return redirect('todo_list')
+
+    context = {'form': form}
+    return render(request, 'main/update_todo.html', context)
