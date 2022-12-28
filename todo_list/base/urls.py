ed9315e9fecf2dab1ca14e939a9851ed06fdc4d1
Vlad <strixus480@gmail.com>
2022-12-28 22:32:48 +0100

    v0.1.7
    -

--- todo_list/base/urls.py
+++ todo_list/base/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, DeleteView, CustomLoginVIew
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('task-create/', TaskCreate.as_view(), name='task-create'),
     path('task-update/<int:pk>', TaskUpdate.as_view(), name='task-update'),
     path('task-delete/<int:pk>', DeleteView.as_view(), name='task-delete'),
+    path('login/', CustomLoginVIew.as_view() ,name='login'),
 
 ]
